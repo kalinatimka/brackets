0f71128534f954a13a7001b5996fde94d2ee9c8d
Timofey <kalinata0799@gmail.com>
2019-09-24 09:32:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    var stack = [];
+    for (var i = 0; i< str.length; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                    if (stack.indexOf(bracketsConfig[j][0]) != -1) {
+                        while (stack.length) {
+                            if (stack.pop() == bracketsConfig[j][0]) {
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                stack.push(str[i]);
+                break;
+            }
+            else if (str[i] == bracketsConfig[j][1]) {
+                if (!stack.length) {
+                    return false;
+                }
+                else {
+                    while (stack.length) {
+                        if (stack.pop() == bracketsConfig[j][0]) {
+                            break;
+                        }
+                    }
+                }
+            }      
+        }
+    }
+    if (!stack.length) {
+        return true;
+    }
+    return false;
 }

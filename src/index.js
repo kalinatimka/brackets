9e320abb25d81f6c6a9cc2c3e98b491eae028417
Timofey <kalinata0799@gmail.com>
2019-10-14 20:34:56 +0300

    fix: add check for not-null stack

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
     // your solution
     var stack = [];
-    for (var i = 0; i< str.length; i++) {
+    for (var i = 0; i< str.length; i++) { 
         for (var j = 0; j < bracketsConfig.length; j++) {
-            if (str[i] == bracketsConfig[j][0]) {
+            if (str[i] == bracketsConfig[j][0]) { 
                 if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
                     if (stack.indexOf(bracketsConfig[j][0]) != -1) {
-                        while (stack.length) {
-                            if (stack.pop() == bracketsConfig[j][0]) {
+                        while (stack.length) { 
+                            if (stack.pop() == bracketsConfig[j][0]) { 
                                 break;
                             }
                         }
@@ -18,14 +18,19 @@ module.exports = function check(str, bracketsConfig) {
                 break;
             }
             else if (str[i] == bracketsConfig[j][1]) {
-                if (!stack.length) {
-                    return false;
+                if (!stack.length) { 
+                    return false; 
                 }
                 else {
-                    while (stack.length) {
-                        if (stack.pop() == bracketsConfig[j][0]) {
-                            break;
+                    if (stack.indexOf(bracketsConfig[j][0]) != -1) { 
+                        while (stack.length) {
+                            if (stack.pop() == bracketsConfig[j][0]) { 
+                                break;
+                            }
                         }
+                    }
+                    else { 
+                        return false;
                     }
                 }
             }      
@@ -36,3 +41,46 @@ module.exports = function check(str, bracketsConfig) {
     }
     return false;
 }
+
+// clean code
+
+// var stack = [];
+// for (var i = 0; i< str.length; i++) {
+//     for (var j = 0; j < bracketsConfig.length; j++) {
+//         if (str[i] == bracketsConfig[j][0]) {
+//             if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+//                 if (stack.indexOf(bracketsConfig[j][0]) != -1) {
+//                     while (stack.length) {
+//                         if (stack.pop() == bracketsConfig[j][0]) {
+//                             break;
+//                         }
+//                     }
+//                     break;
+//                 }
+//             }
+//             stack.push(str[i]);
+//             break;
+//         }
+//         else if (str[i] == bracketsConfig[j][1]) {
+//             if (!stack.length) {
+//                 return false;
+//             }
+//             else {
+//                 if (stack.indexOf(bracketsConfig[j][0]) != -1) {
+//                     while (stack.length) {
+//                         if (stack.pop() == bracketsConfig[j][0]) {
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 else {
+//                     return false;
+//                 }
+//             }
+//         }      
+//     }
+// }
+// if (!stack.length) {
+//     return true;
+// }
+// return false;
